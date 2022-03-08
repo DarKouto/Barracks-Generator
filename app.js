27ee8590e71cac24bdb5762391b7da68a71e6ea5
Daniel Couto <54581486+DarKouto@users.noreply.github.com>
2022-03-08 17:12:34 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,67 @@
+const cheiros = [
+  "leite Creme","leite azedo","leite achocolatado","leite condensado",
+  "presunto curado",
+  "veneno para ratos",
+  "maçãs podres",
+  "chulé de chinês",
+  "manteiga de amendoim",
+  "pickles",
+  "WC Pato",
+  "fiambre da perna extra",
+  "queijo da serra","queijo Flamengo",
+  "pão recesso","pão de forma","pão rústico",
+  "coentros estragados",
+  "bacalhau fora do prazo",
+  "framboesa podre",
+  "beterraba",
+  "tremoços secos",
+  "merda fossilizada"
+];
+const locais = [
+  "da cabeça",
+  "do cabelo",
+  "da testa",
+  "das sobrancelhas",
+  "das pestanas",
+  "dos olhos",
+  "dos ouvidos",
+  "das orelhas",
+  "do nariz",
+  "da boca",
+  "dos lábios",
+  "das gengivas",
+  "dos dentes",
+  "da garganta",
+  "dos ombros",
+  "das axilas",
+  "dos sovacos",
+  "dos braços",
+  "dos cotovelos",
+  "do antebraço",
+  "das mãos",
+  "dos dedos","dos dedos dos pés",
+  "das unhas","das unhas dos pés",
+  "da barriga",
+  "das costas",
+  "dos mamilos",
+  "do umbigo",
+  "do cu",
+  "das pernas",
+  "dos joelhos",
+  "das coxas",
+  "dos gémeos",
+  "dos tornozelos"
+];
+
+botao = document.getElementById('botao');
+
+botao.onclick = function() {
+  //randomizar
+  var cheiro = cheiros[Math.floor(Math.random() * cheiros.length)];
+  var local = locais[Math.floor(Math.random() * locais.length)];
+
+  //actualizar página
+  const insulto = document.getElementById("insulto");
+  insulto.innerHTML = "Cheiras a " + cheiro + " " + local;
+  botao.innerHTML = "INSULTA-ME MAIS SUA PORCA BARRAQUEIRA!";
+}
